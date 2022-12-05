0f9eda041629655996c3bb97c5562769867181a3
Ahmed Boulahdjar <ahmed.boulahdjar@ceff.ch>
2022-12-05 13:20:44 +0100

    Add Categories and settings of the Window

---
+++ FiveM-Cache-Cleaner.ps1
@@ -0,0 +1,44 @@
+##############################################################################################################################
+#                                     Création d'un formulaire avec une interface graphique                                  #
+##############################################################################################################################
+
+# Chargement des classe 
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+##############################################################################################################################
+#                                                            Function                                                        #
+##############################################################################################################################
+
+
+##############################################################################################################################
+#                                                     Window Settings                                                        #
+##############################################################################################################################
+
+# Création de la fenêtre pour contenir les éléments
+$main_form = New-Object System.Windows.Forms.Form
+
+#Le titre de la fenêtre
+$main_form.Text ='Formulaire CEFF'
+
+# Largeur de la fenêtre
+$main_form.Width = 400
+
+# Hauteur de la fenêtre
+$main_form.Height = 400
+
+# Étire automatiquement la fenêtre
+$main_form.AutoSize = $true
+
+#Couleur du fond
+$main_form.BackColor = "54,75,109"
+
+# Bloque la taille max et min
+$main_form.minimumSize = New-Object System.Drawing.Size(965,665)
+$main_form.maximumSize = New-Object System.Drawing.Size(965,665)
+
+##############################################################################################################################
+#                                                       Design                                                               #
+##############################################################################################################################
+
+# Je suis pas designer
